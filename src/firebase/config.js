a0f1b02fca467426c4ee7057c52c8aa89de22a72
Ebrar <16155052@mersin.edu.tr>
2023-04-09 17:04:05 +0300

    last commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,14 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyABzWtJNyInRsJwJUOJa13uspumVEAyNs8",
-  authDomain: "blog-vue-e73fa.firebaseapp.com",
-  projectId: "blog-vue-e73fa",
-  storageBucket: "blog-vue-e73fa.appspot.com",
-  messagingSenderId: "531121837989",
-  appId: "1:531121837989:web:c8be9076553786103a7a4c"
-};
 
 // Initialize Firebase
 const fb = initializeApp(firebaseConfig);
